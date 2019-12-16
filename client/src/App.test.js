f5549bb10e14b152e3ec465391c8cdb7bfc46cb3
Kevin Grant <ConsciousK@yahoo.com>
2019-12-16 18:35:28 -0500

    Completed MVP of Sprint Challenge.

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import App from "./App";
+import Navbar from "./components/Navbar";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test("App renders without crashing", () => {
+  const component = render(<App />);
+  console.log(component);
+});
+
+test("Navbar renders without crashing", () => {
+  const component = render(<Navbar />);
+});
+
+test("Is there a playersCont", () => {
+  const { findAllByDisplayValue } = render(<App />);
+
+  findAllByDisplayValue(/playersCont/i);
+});
+
+test("Is there a toggle", () => {
+  const { findAllByDisplayValue } = render(<App />);
+
+  findAllByDisplayValue(/toggle/i);
+});
+
+test("Is there text on screen", () => {
+  const { getByText } = render(<App />);
+  expect(getByText(/world cup players/i));
 });
